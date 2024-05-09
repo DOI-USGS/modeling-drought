73a4d88626518aa110abbbcb971301915492d818
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-05-09 08:52:46 -0500

    start updating project-specific attributions

--- src/assets/text/authors.js
+++ src/assets/text/authors.js
@@ -3,91 +3,18 @@ export default {
     // currently only 'fullName', 'firstName', 'initials', 'profile_link', and 'contribution' are used
     primaryAuthors: [
       {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'led the design and development of the website'
-      }
-    ],
-    // do not delete section. delete any or all individuals as needed. modify content as needed
-    // currently only 'fullName', 'firstName', 'initials', 'profile_link', and 'contribution' are used
-    additionalAuthors: [
-      {
-        firstName: 'Althea',
-        lastName: 'Archer',
-        fullName: 'Althea A. Archer',
-        initials: 'AAA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
-        role: 'contributor',
-        contribution: 'contributed to website development, created data visualizations and reviewed code'
-      },
-      {
-        firstName: 'Elmera',
-        lastName: 'Azadpour',
-        fullName: 'Elmera Azadpour',
-        initials: 'EA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/elmera-azadpour',
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Mandie',
-        lastName: 'Carr',
-        fullName: 'Mandie Carr',
-        initials: 'MC',
-        profile_link: 'https://www.usgs.gov/staff-profiles/amanda-carr',
-        role: 'contributor',
-        contribution: 'wrote the narrative for the site'
-      },
-      {
-        firstName: 'Aileen',
-        lastName: 'Clarke',
-        fullName: 'Aileen Clarke',
-        initials: 'AC',
-        profile_link: 'https://www.usgs.gov/staff-profiles/aileen-m-clarke',
-        role: 'contributor',
-        contribution: 'contributed to website development'
-      },
-      {
-        firstName: 'Leo',
-        lastName: 'Ivey',
-        fullName: 'Leo Ivey',
-        initials: 'LI',
-        profile_link: null,
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Maggie',
-        lastName: 'Jaenicke',
-        fullName: 'Maggie Jaenicke',
-        initials: 'MJ',
-        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
-        role: 'contributor',
-        contribution: 'contributed to website development'
-      },
-      {
         firstName: 'Jeffrey',
         lastName: 'Kwang',
         fullName: 'Jeffrey Kwang',
         initials: 'JK',
         profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang',
-        role: 'contributor',
+        role: 'lead',
         contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Anthony',
-        lastName: 'Martinez',
-        fullName: 'Anthony Martinez',
-        initials: 'AM',
-        profile_link: 'https://www.usgs.gov/staff-profiles/anthony-martinez',
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
+      }
+    ],
+    // do not delete section. delete any or all individuals as needed. modify content as needed
+    // currently only 'fullName', 'firstName', 'initials', 'profile_link', and 'contribution' are used
+    additionalAuthors: [
       {
         firstName: 'Cee',
         lastName: 'Nell',
@@ -98,13 +25,23 @@ export default {
         contribution: 'acted in an advisory role, contributed to website development, and reviewed code'
       },
       {
-        firstName: 'Kaysa',
-        lastName: 'Vaarre-Lamoureux',
-        fullName: 'Kaysa Vaarre-Lamoureux',
-        initials: 'KVL',
+        firstName: 'Hayley',
+        lastName: 'Corson-Dosch',
+        fullName: 'Hayley Corson-Dosch',
+        initials: 'HCD',
+        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        
+        role: 'developer',
+        contribution: 'built the structure of the website and reviewed code'
+      },
+      {
+        firstName: 'Jeremy',
+        lastName: 'Diaz',
+        fullName: 'Jeremy Diaz',
+        initials: 'JD',
         profile_link: null,
-        role: 'contributor',
-        contribution: 'created data visualizations'
+        role: 'SME',
+        contribution: 'provided subject matter expertise on modeling and uncertainty quantification'
       }
     ]
 };
