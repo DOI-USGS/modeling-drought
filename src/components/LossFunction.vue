ab4d70ccf460afd5a86f6adcd9cc788884ddd6d1
Corson-Dosch <hcorson-dosch@usgs.gov>
2025-04-08 11:37:35 -0500

    cleanup and minor tweaks

--- src/components/LossFunction.vue
+++ src/components/LossFunction.vue
@@ -14,13 +14,15 @@
     <template #aboveExplanation>
       <p v-html="text.paragraph1" />
       <p v-html="text.paragraph2" />
-      <ToggleSwitch 
-        v-for="layer, index in layers"
-        :key="index"
-        v-model="layer.visible" 
-        :label="layer.label"
-        :right-color="layer.color"
-      />
+      <div class="toggle-container">
+        <ToggleSwitch 
+          v-for="layer, index in layers"
+          :key="index"
+          v-model="layer.visible" 
+          :label="layer.label"
+          :right-color="layer.color"
+        />
+      </div>
     </template>
     <template #figures>
       <div id="lf-grid-container">
